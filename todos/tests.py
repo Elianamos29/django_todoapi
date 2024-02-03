e6b7bc6468d0f5d288997f132bc7523a6636baf8
Elianamos <elianamos29@gmail.com>
2024-02-03 11:42:50 +0300

    write test for todo model

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title = "first todo",
+            body = "a body here"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "first todo")
+        self.assertEqual(self.todo.body, "a body here")
+        self.assertEqual(str(self.todo), "first todo")
