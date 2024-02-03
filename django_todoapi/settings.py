ca308e48942288c39d9a7eb23dad27b7a59a9c5b
Elianamos <elianamos29@gmail.com>
2024-02-03 11:55:07 +0300

    configured todos urls

--- django_todoapi/settings.py
+++ django_todoapi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
+    "rest_framework",
     # Local
     "todos.apps.TodosConfig",
 ]
@@ -123,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny"
+    ]
+}
