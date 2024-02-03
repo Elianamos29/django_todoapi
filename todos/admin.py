9e0327469b05680fed9da29f08875b2174099b35
Elianamos <elianamos29@gmail.com>
2024-02-03 11:36:02 +0300

    created todo model

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+    )
+
+admin.site.register(Todo, TodoAdmin)
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
