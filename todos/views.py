73e7e11f1209bebe24d89c4b40f438618ee31a8e
Elianamos <elianamos29@gmail.com>
2024-02-03 12:05:52 +0300

    create the api view

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
