5c1e9c5b69af26401ddf2d94fe8e811c287c0eb6
Elianamos <elianamos29@gmail.com>
2024-02-04 09:46:53 +0300

    write test for the api

--- todos/tests.py
+++ todos/tests.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 from .models import Todo
 
 class TodoModelTest(TestCase):
@@ -13,3 +17,18 @@ class TodoModelTest(TestCase):
         self.assertEqual(self.todo.title, "first todo")
         self.assertEqual(self.todo.body, "a body here")
         self.assertEqual(str(self.todo), "first todo")
+    
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+    
+    def test_api_detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}),
+            format= "json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "first todo")
